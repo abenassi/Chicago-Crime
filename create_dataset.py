0735e1622ef562e45608f1bea953ca7e95c7e22f
Agustín Benassi <agusbenassi@gmail.com>
2015-06-08 19:25:34 -0300

    Download crime dataset if not already in "datasets" folder.

--- create_dataset.py
+++ create_dataset.py
@@ -12,6 +12,8 @@ and collapse it by Community Area, Primary Type (of crime) and Year-Month.
 from __future__ import unicode_literals
 import pandas as pd
 import arrow
+import requests
+import os
 
 
 def generate_year_month(str_date):
@@ -21,7 +23,47 @@ def generate_year_month(str_date):
     return arrow.get(date.year, date.month, 1).datetime
 
 
+def download_dataset():
+    """Download a dataset in the "datasets" folder if not already done."""
+
+    name = "Crimes_-_2001_to_present.csv"
+    path = os.path.join("datasets", name)
+
+    # not being used because it's too slow to start..
+    original_url = "https://data.cityofchicago.org/api/views/ijzp-q8t2/rows.csv?accessType=DOWNLOAD"
+
+    # dropbox share link
+    url = "https://www.dropbox.com/s/otvhgqo8ftjkd0b/Crimes_-_2001_to_present.csv?dl=1"
+
+    if False and os.path.isfile(path):
+        print "Dataset was already downloaded!"
+
+    else:
+        try:
+            print "Requesting download..."
+            response = requests.get(url, stream=True)
+            print "Starting download..."
+            total_length = int(response.headers.get('content-length'))
+
+            progress = 0.0
+            with open(path, 'wb') as f:
+                for data in response.iter_content(1024000):
+                    f.write(data)
+                    progress += len(data)
+                    print "Progress", "{:.2f}%".format(progress /
+                                                       total_length * 100)
+
+            print "\nDataset successfully downloaded."
+
+        except Exception as inst:
+            print "Dataset couldn't be downloaded because of", inst
+
+
 def main():
+
+    print "Downloading Crimes_-_2001_to_present.csv, this will take time..."
+    download_dataset()
+
     print "Reading csv dataset into a pandas data frame..."
     df = pd.read_csv("./datasets/Crimes_-_2001_to_present.csv")
 
